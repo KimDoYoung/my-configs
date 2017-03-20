1d7560c266e0d9f2e0249ef5886c4097b3b26d7b
KimDoYoung <kdy987@gmail.com>
2017-03-20 19:51:29 +0900

    "2017-03-20 modify"

--- shell/collect.bash
+++ shell/collect.bash
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 declare -a files=("$HOME/.vimrc" "$HOME/.bashrc" "$HOME/.bash_profile")
-declare -r TARGET="$HOME/Documents/my-configs/tmp"
-for file in "${files[@]}" 
+pwd=$(pwd)
+parentdir=$(dirname "${pwd}")
+targetdir="${parentdir}/tmp"
+if [[  ! -e ${targetdir} ]]; then
+		mkdir -p "${targetdir}"
+fi
+for file in "${files[@]}"
 do
-	cp "${file}" "${TARGET}" 
-	printf "%s\n" "${file}"
+	if [[ -f "${file}" ]]; then
+	cp "${file}" "${targetdir}"
+	printf "copied %s\n" "${file}"
+	else
+  printf "%s is not found\n" "${file}"
+	fi
 done
-ls -a ${TARGET}
